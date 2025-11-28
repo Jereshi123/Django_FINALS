37101351e92d8d192c2a07e3f1fcb44671cedc15
Pozon_Jerome <pozonjeromeandre@gmail.com>
2025-11-28 09:39:57 +0800

    Changes done.

--- core/models.py
+++ core/models.py
@@ -3,21 +3,17 @@ from django.utils import timezone
 
 
 class SoftDeleteManager(models.Manager):
-    """Manager that filters out soft-deleted records by default"""
     def get_queryset(self):
         return super().get_queryset().filter(deleted_at__isnull=True)
 
     def all_with_deleted(self):
-        """Get all records including deleted ones"""
         return super().get_queryset()
 
     def only_deleted(self):
-        """Get only soft-deleted records"""
         return super().get_queryset().filter(deleted_at__isnull=False)
 
 
 class SoftDeleteModel(models.Model):
-    """Abstract base model that adds soft delete functionality"""
     deleted_at = models.DateTimeField(null=True, blank=True, editable=False)
     
     objects = SoftDeleteManager()
@@ -26,15 +22,12 @@ class SoftDeleteModel(models.Model):
         abstract = True
     
     def soft_delete(self):
-        """Soft delete the record"""
         self.deleted_at = timezone.now()
         self.save(update_fields=['deleted_at'])
     
     def restore(self):
-        """Restore a soft-deleted record"""
         self.deleted_at = None
         self.save(update_fields=['deleted_at'])
     
     def is_deleted(self):
-        """Check if record is soft-deleted"""
         return self.deleted_at is not None
